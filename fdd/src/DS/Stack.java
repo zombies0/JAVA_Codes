75dae6d06e23bb97734c69ba9b3b4035b152905f
Ambrish <ambrish@Ambrishs-iMac-Pro.local>
2021-04-08 20:23:21 +0530

    Data Structures folder added

---
+++ fdd/src/DS/Stack.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Stack {
+
+    public static void main(String[] args) {
+        
+        
+        int [] array = new int[5];
+
+        System.out.println(Arrays.toString(array));
+
+
+    }
+    
+}
